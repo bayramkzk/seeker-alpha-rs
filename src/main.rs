18058206b77e9c6a5b6a4a96be7d9014de09532d
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-03-09 06:15:44 +0300

    Install bare minimum dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenvy::dotenv().expect("No .env file found");
+    let db_url = dotenvy::var("DATABASE_URL").expect("No DATABASE_URL found");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(db_url.as_str())
+        .await?;
+
+    let row: (String,) = sqlx::query_as("SELECT url FROM call_pages")
+        .fetch_one(&pool)
+        .await?;
+
+    let ip = reqwest::get("https://ip.oxylabs.io").await?.text().await?;
+    let ip = ip.trim_end();
+
+    println!("IP: {}", ip);
+    println!("URL: {}", row.0);
+
+    Ok(())
 }
